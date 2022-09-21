7242c2b4a98c5c504d16bc6f8911fadab018f374
Fabien Roy <froy@jstrategia.com>
2022-09-21 17:38:36 -0400

    chore: moar eslint config

--- packages/eslint-config-custom/base.js
+++ packages/eslint-config-custom/base.js
@@ -1,6 +1,10 @@
+// TODO: Prettier (plugin)
+// TODO: Import (plugin)
 module.exports = {
   plugins: ["functional"],
   extends: [
+    'plugin:prefer-type-alias/recommended',
+    'plugin:import/recommended',
     'plugin:functional/recommended',
     'plugin:functional/stylistic',
     'eslint:recommended',
@@ -8,9 +12,28 @@ module.exports = {
     "turbo"
   ],
   rules: {
+    curly: 'warn',
+    eqeqeq: 'warn',
+    'prefer-template': 'warn',
+    'func-style': ['warn', 'declaration'],
+    'no-negated-condition': 'warn',
+    'no-else-return': 'warn',
+    'no-nested-ternary': 'warn',
+    'import/order': 'warn',
+    'import/first': 'warn',
+    'import/exports-last': 'warn',
     'functional/no-class': 'error',
     'functional/no-return-void': 'off',
     'functional/no-expression-statement': 'off',
     'functional/functional-parameters': 'off',
   },
+  overrides: [
+    {
+      files: ['**/*.js'],
+      rules: {
+        'functional/no-expression-statement': 'off',
+        'functional/immutable-data': 'off',
+      },
+    },
+  ],
 };
--- packages/web-ui/.eslintrc.js
+++ packages/web-ui/.eslintrc.js
@@ -3,4 +3,8 @@ module.exports = {
   root: true,
   parser: "babel-eslint",
   extends: ["custom/react.js"],
+  rules: {
+    // TODO: Very weird, we shouldn't have to do that
+    'import/no-unresolved': 'off'
+  }
 };
