e85e54b3201839103d382fc1e828692fcc929f99
Fabien Roy <froy@jstrategia.com>
2022-09-21 17:23:20 -0400

    chore: add eslint functional plugin

--- packages/eslint-config-custom/base.js
+++ packages/eslint-config-custom/base.js
@@ -1,6 +1,17 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  plugins: ["functional"],
+  extends: [
+    'plugin:functional/recommended',
+    'plugin:functional/stylistic',
+    "prettier",
+    "next",
+    "turbo"
+  ],
   rules: {
+    'functional/no-class': 'error',
+    'functional/no-return-void': 'off',
+    'functional/no-expression-statement': 'off',
+    'functional/functional-parameters': 'off',
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
   },
