db0da905c285a041ab52d710504bfe8467869b6f
Fabien Roy <fabienroy28@gmail.com>
2022-09-21 22:30:07 -0400

    chore: cleanup

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,16 +1,16 @@
-import { Button } from "web-ui/components/inputs";
-import { Header1, Header2 } from "web-ui/components/typography";
-import { useThemeContext } from "web-ui/contexts/ThemeProvider";
+import { Button } from 'web-ui/components/inputs';
+import { Header1, Header2 } from 'web-ui/components/typography';
+import { useThemeContext } from 'web-ui/contexts/ThemeProvider';
 
 function Index() {
-  const {switchMode} = useThemeContext();
+  const { switchMode } = useThemeContext();
 
   return (
-    <div>
+    <>
       <Header1>TreeView</Header1>
       <Header2>Index</Header2>
       <Button onClick={switchMode}>Press me!</Button>
-    </div>
+    </>
   );
 }
 
