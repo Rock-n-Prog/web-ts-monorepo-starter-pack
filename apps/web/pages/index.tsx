51bd5cfebe87341a7d5218eef7c4e7f8c78637a9
Fabien Roy <froy@jstrategia.com>
2022-09-22 14:56:38 -0400

    feat: use static props in index

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,12 +1,16 @@
 import { Button } from 'web-ui/components/inputs';
 import { Header1, Header2, Body1 } from 'web-ui/components/typography';
 import { useThemeContext } from 'web-ui/hooks/useThemeContext';
+import { createProxySSGHelpers } from '@trpc/react/ssg';
+import superjson from 'superjson';
 import { trpc } from '../utils/trpc';
+import { createContext } from '../server/context';
+import { appRouter } from '../server/routers/_app';
 
 function Index() {
   const { switchMode } = useThemeContext();
   // TODO: Why does this keep refreshing? See console logs
-  const { data } = trpc.hello.single.useQuery({ text: 'client' });
+  const { data } = trpc.hello.single.useQuery({ text: 'TreeView' });
 
   return (
     <>
@@ -18,4 +22,22 @@ function Index() {
   );
 }
 
+async function getStaticProps() {
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    transformer: superjson,
+    ctx: createContext(),
+  });
+
+  await ssg.hello.single.fetch({ text: 'TreeView' });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+    },
+    revalidate: 1,
+  };
+}
+
+export { getStaticProps };
 export default Index;
