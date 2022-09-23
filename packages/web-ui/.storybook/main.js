fea4491a7293621593542d0647463acf07dae69f
Fabien Roy <froy@jstrategia.com>
2022-09-23 13:04:02 -0400

    docs: add TODO

--- packages/web-ui/.storybook/main.js
+++ packages/web-ui/.storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-links',
     '@storybook/addon-interactions',
+    // TODO: Deprecated: https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#deprecated-addon-contexts
     '@storybook/addon-contexts',
   ],
   framework: '@storybook/react',
