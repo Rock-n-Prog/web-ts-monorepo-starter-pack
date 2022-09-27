756b54d6ea08b9aa9a95dbd7b3f5231c0f7ddb94
Fabien Roy <froy@jstrategia.com>
2022-09-27 08:45:33 -0400

    chore: run storybook on correct directory

--- apps/mobile-storybook/App.tsx
+++ apps/mobile-storybook/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Storybook from './storybook';
+import Storybook from './.storybook';
 
 // TODO: Add theme provider
 // TODO: Same SafeAreaView as mobile app?
