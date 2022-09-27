5f091827d6bb593da61990350d7a8c2bae08b18b
Fabien Roy <froy@jstrategia.com>
2022-09-26 22:50:17 -0400

    docs: add TODOs

--- apps/mobile-storybook/storybook/index.js
+++ apps/mobile-storybook/storybook/index.js
@@ -1,5 +1,10 @@
 import { getStorybookUI, configure } from '@storybook/react-native';
+// import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
+// import { withKnobs } from '@storybook/addon-knobs';
 import './rn-addons';
+
+// TODO: Configure same addons as web-ui
+// addDecorator(withKnobs);
 
 // TODO: Use @storybook/react-native-server (helps development)
 // TODO: Dark mode?
