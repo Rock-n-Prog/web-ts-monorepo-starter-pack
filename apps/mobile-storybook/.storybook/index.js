f952d102922968464f236188640cc4e1f3def8ad
Fabien Roy <froy@jstrategia.com>
2022-09-27 09:02:11 -0400

    chore: cleanup deps

--- apps/mobile-storybook/.storybook/index.js
+++ apps/mobile-storybook/.storybook/index.js
@@ -1,13 +1,13 @@
-import { getStorybookUI, configure } from '@storybook/react-native';
-// import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
-// import { withKnobs } from '@storybook/addon-knobs';
+import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
+import { withKnobs } from '@storybook/addon-knobs';
 import './rn-addons';
 
-// TODO: Configure same addons as web-ui
-// addDecorator(withKnobs);
+addDecorator(withKnobs);
 
+// TODO: Storybook v6? knobs -> controls, context -> toolbar (try same as web-ui)
 // TODO: Get @storybook/react-native-server to work (helps development) -> Stories not showing, addons needing to be commented
 //       Update: lol might not be that simple -> https://github.com/storybookjs/react-native/issues/55
+//       Might be for a future upgrade
 // TODO: Dark mode?
 // TODO: Toolbar addon that switches current theme?
 configure(() => {
--- apps/mobile-storybook/App.tsx
+++ apps/mobile-storybook/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Storybook from './.storybook';
 
 // TODO: Add theme provider
-// TODO: Same SafeAreaView as mobile app?
+// TODO: Same SafeAreaView as mobile app? (storybook can't be used properly)
 function App() {
   return <Storybook />;
 }
