e5e310d6f1015b28c354664060a0c9260dc5fb1c
Fabien Roy <froy@jstrategia.com>
2022-09-26 22:47:47 -0400

    docs: add TODO

--- apps/mobile-storybook/storybook/index.js
+++ apps/mobile-storybook/storybook/index.js
@@ -1,6 +1,7 @@
 import { getStorybookUI, configure } from '@storybook/react-native';
 import './rn-addons';
 
+// TODO: Use @storybook/react-native-server (helps development)
 // TODO: Dark mode?
 // TODO: Toolbar addon that switches current theme?
 configure(() => {
