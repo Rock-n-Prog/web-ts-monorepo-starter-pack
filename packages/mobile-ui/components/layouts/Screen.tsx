647a4ecfb88ab0a89aa0f26a17033adbd80e3607
Fabien Roy <froy@jstrategia.com>
2022-09-27 22:23:35 -0400

    fix: repair screen

--- packages/mobile-ui/components/layouts/Screen.tsx
+++ packages/mobile-ui/components/layouts/Screen.tsx
@@ -2,32 +2,12 @@ import * as React from 'react';
 import styled, { css } from 'styled-components/native';
 import { Theme } from '../../styles/theme';
 
-type ScreenProps = {
-  fullHeight?: boolean;
-  fullWidth?: boolean;
-};
-
-function Screen({ fullHeight = false, fullWidth = false, children }: React.PropsWithChildren<ScreenProps>) {
-  return (<Wrapper><StyledScreen $fullHeight={fullHeight} $fullWidth={fullWidth}>{children}</StyledScreen></Wrapper>);
-}
-
-const Wrapper = styled.View(
+const Screen = styled.View(
   ({ theme }: { readonly theme: Theme }) => css`
     background-color: ${theme.colors.background};
-    height: 100%;
-  `,
-);
-
-type StyledScreenProps = {
-  readonly $fullHeight: boolean;
-  readonly $fullWidth: boolean;
-  readonly theme: Theme;
-};
-
-const StyledScreen = styled.View(
-  ({ $fullHeight, $fullWidth, theme }: StyledScreenProps) => css`
-    ${!$fullWidth && `padding-horizontal: ${theme.spacing.l}`}
-    ${!$fullHeight && `padding-vertical: ${theme.spacing.l}`}
+    padding: ${theme.spacing.l};
+    flex: 1;
+    align-items: center;
   `,
 );
 
