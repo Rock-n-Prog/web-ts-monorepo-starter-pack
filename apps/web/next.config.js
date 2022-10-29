4644c1b18e2331e95cb802c8d6cbf6be33ef76eb
Fabien Roy <froy@jstrategia.com>
2022-10-29 12:08:34 -0400

    chore: cleanup

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,4 @@
-const withTM = require('next-transpile-modules')(['database', 'trpc', 'web-ui', 'theme']);
+const withTM = require('next-transpile-modules')(['trpc', 'web-ui', 'theme']);
 
 module.exports = withTM({
   reactStrictMode: true,
