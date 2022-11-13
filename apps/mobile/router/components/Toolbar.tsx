69f0345d6226288b3ebc29211ed069948894e466
Fabien Roy <fabienroy28@gmail.com>
2022-11-13 12:29:28 -0500

    feat: use horizontal stack

--- apps/mobile/router/components/Toolbar.tsx
+++ apps/mobile/router/components/Toolbar.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { Button } from "mobile-ui/components/inputs";
-import { useThemeContext } from "mobile-ui/hooks/useThemeContext";
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
+import { Button } from 'mobile-ui/components/inputs';
+import { useThemeContext } from 'mobile-ui/hooks/useThemeContext';
+import { Stack } from 'mobile-ui/components/layouts';
 
-// TODO: Stack, horizontal
 // TODO: Toolbar should be a dev tools menu (side panel/blade)
 function Toolbar() {
   const {t} = useTranslation();
@@ -11,10 +11,10 @@ function Toolbar() {
 
   // TODO: Switch language
   return (
-    <>
+    <Stack direction="row">
       <Button text={t('language')} onPress={switchMode} />
       <Button text={t('theme')} onPress={switchMode} />
-    </>
+    </Stack>
   );
 }
 
--- packages/mobile-ui/components/layouts/Stack.tsx
+++ packages/mobile-ui/components/layouts/Stack.tsx
@@ -3,44 +3,59 @@ import { View } from 'react-native';
 import styled, { css } from "styled-components/native";
 import { Theme } from "../../styles/theme";
 
+type Direction = 'row' | 'column';
+
 type StackProps = {
   readonly children: readonly React.ReactNode[];
   readonly alignCenter?: boolean;
+  readonly direction?: Direction;
 };
 
 // TODO: Stories
-function Stack({ children, alignCenter = true }: StackProps) {
+function Stack({ children, direction = 'column', alignCenter = true }: StackProps) {
   return (
-    <Container $alignCenter={alignCenter}>
-      {children.map((child, key) => (
-        <Item key={key} $spaceTop={key !== 0}>{child}</Item>
-      ))}
+    <Container $direction={direction} $alignCenter={alignCenter}>
+      {children.map((child, key) => <Item key={key} $direction={direction} $isFirst={key === 0}>{child}</Item>)}
     </Container>
   )
 }
 
 type ContainerProps = {
+  readonly $direction: Direction;
   readonly $alignCenter: boolean;
   readonly theme: Theme;
 }
 
 const Container = styled(View)(
-  ({ $alignCenter, theme }: ContainerProps) => css`
+  ({ $direction, $alignCenter, theme }: ContainerProps) => css`
     display: flex;
-    align-items: ${$alignCenter ? 'center' : undefined};
+    flex-direction: ${$direction};
     padding: ${theme.spacing.xxs} ${theme.spacing.m};
+
+    ${$alignCenter && `
+      ${$direction === 'column' && `
+        align-items: center;
+      `}
+
+      ${$direction === 'row' && `
+        justify-content: center;
+      `}
+    `}
   `,
 );
 
 type ItemProps = {
-  readonly $spaceTop: boolean;
+  readonly $direction: Direction;
+  readonly $isFirst: boolean;
   readonly theme: Theme;
 }
 
 const Item = styled(View)(
-  ({ $spaceTop, theme }: ItemProps) => css`
+  ({ $direction, $isFirst, theme }: ItemProps) => css`
     display: flex;
-    margin-top: ${$spaceTop ? theme.spacing.s : 0};
+
+    margin-top: ${!$isFirst && $direction === 'column' ? theme.spacing.s : 0};
+    margin-left: ${!$isFirst && $direction === 'row' ? theme.spacing.s : 0};
   `,
 );
 
