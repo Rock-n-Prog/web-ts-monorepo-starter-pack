60f31e427e634827ca761c6d623f79dfbda939d2
Fabien Roy <fabienroy28@gmail.com>
2022-11-14 15:26:20 -0500

    feat: use snackbars

--- apps/mobile/screens/users/components/AddUserForm.tsx
+++ apps/mobile/screens/users/components/AddUserForm.tsx
@@ -7,16 +7,18 @@ import { addUserInputSchema } from 'trpc/schemas';
 // TODO: web-ui is "layout" instead of "layouts"
 import { Stack } from 'mobile-ui/components/layouts';
 import { Button } from 'mobile-ui/components/inputs';
+import { useSnackbarContext } from 'mobile-ui/hooks/useSnackbarContext';
 import { trpc } from '../../../utils/trpc';
 import { TextField } from '../../../components/forms';
 
 // TODO: Keyboard avoiding view?
 function AddUserForm() {
+  const { showSnackbar } = useSnackbarContext();
   const { t } = useTranslation('users', { keyPrefix: 'form' });
+  // TODO: Mutate does not work?
   const { mutate } = trpc.users.add.useMutation({
-    // TODO: Show snackbar
-    onSuccess: () => console.log({ text: t('newUserAdded'), severity: 'success' }),
-    onError: error => console.error({ text: error.message, severity: 'error' }),
+    onSuccess: () => showSnackbar({ text: t('newUserAdded'), severity: 'success' }),
+    onError: error => showSnackbar({ text: error.message, severity: 'error' }),
   });
   const {
     handleSubmit,
@@ -31,6 +33,7 @@ function AddUserForm() {
     },
   });
 
+  // TODO: Next input press? Same in web, with tab? (see: https://stackoverflow.com/questions/32748718/react-native-how-to-select-the-next-textinput-after-pressing-the-next-keyboar)
   return (
     <Stack>
       <TextField label={t('name')} control={control} name="name" />
