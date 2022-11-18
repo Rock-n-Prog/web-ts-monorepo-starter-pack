e8918c88ffcd0af82403bdd75b79c41d5e023478
Fabien Roy <fabienroy28@gmail.com>
2022-11-18 17:51:41 -0500

    chore: cleanup

--- apps/mobile/i18n.ts
+++ apps/mobile/i18n.ts
@@ -1,11 +1,11 @@
 import * as i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import 'intl-pluralrules';
 import * as en from '@acme/mobile-locales/translations/en';
 import * as fr from '@acme/mobile-locales/translations/fr';
 import { defaultLocale } from '@acme/mobile-locales/types/locales';
 import { defaultNamespace, namespaces } from '@acme/mobile-locales/types/namespaces';
 import { languageDetector } from '@acme/mobile-locales/plugins/languageDetector';
+import 'intl-pluralrules';
 
 i18n
   .use(initReactI18next)
