016415ce856520f25db98c8eb69e926a5c5ea76a
Fabien Roy <fabienroy28@gmail.com>
2022-11-19 10:49:37 -0500

    fix: repair api lint

--- packages/api/.eslintrc.js
+++ packages/api/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: ['@acme/eslint-config/node.js'],
+  rules: {
+    // Weird fix for database not being found by lint
+    'import/no-unresolved': 'off',
+  },
 };
