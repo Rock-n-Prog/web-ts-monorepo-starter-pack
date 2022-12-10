e0bfbf675c325e8df7376816faa00a43073fae8d
Fabien Roy <fabienroy28@gmail.com>
2022-12-10 15:10:34 -0500

    chore: add feedback screen

--- apps/mobile-ui-showcase/screens/feedback/FeedbackScreen.tsx
+++ apps/mobile-ui-showcase/screens/feedback/FeedbackScreen.tsx
@@ -1,12 +1,44 @@
 import * as React from 'react';
-import { Body1 } from '@acme/mobile-ui/components/typography';
+import { Body1, SubTitle } from '@acme/mobile-ui/components/typography';
+import { Alert } from '@acme/mobile-ui/components/feedback';
 import { Screen } from '@acme/mobile-ui/components/layouts';
+import { useSnackbarContext } from '@acme/mobile-ui/hooks/useSnackbarContext';
+import { Button } from '@acme/mobile-ui/components/inputs';
+import { Severity } from '@acme/mobile-ui/types/severity';
+import { ScrollView } from 'react-native';
 
 function FeedbackScreen() {
-  // TODO: Add sub-nav
+  const { showSnackbar } = useSnackbarContext();
+  const [number, setNumber] = React.useState(1);
+
+  function handlePress(severity: Severity) {
+    showSnackbar({ text: `Snackbar #${number}`, severity });
+    setNumber(number + 1);
+  }
+
   return (
     <Screen>
-      <Body1>Feedback!</Body1>
+      <ScrollView>
+        <SubTitle>Alert</SubTitle>
+        <Body1>Info</Body1>
+        <Alert text="Alert text" severity="info" />
+        <Body1>Success</Body1>
+        <Alert text="Alert text" severity="success" />
+        <Body1>Warning</Body1>
+        <Alert text="Alert text" severity="warning" />
+        <Body1>Error</Body1>
+        <Alert text="Alert text" severity="error" />
+
+        <SubTitle>Snackbar</SubTitle>
+        <Body1>Info</Body1>
+        <Button text="Show snackbar" onPress={() => handlePress('info')} />
+        <Body1>Success</Body1>
+        <Button text="Show snackbar" onPress={() => handlePress('success')} />
+        <Body1>Warning</Body1>
+        <Button text="Show snackbar" onPress={() => handlePress('warning')} />
+        <Body1>Error</Body1>
+        <Button text="Show snackbar" onPress={() => handlePress('error')} />
+      </ScrollView>
     </Screen>
   );
 }
--- packages/mobile/ui/components/typography/SubTitle.tsx
+++ packages/mobile/ui/components/typography/SubTitle.tsx
@@ -3,11 +3,11 @@ import styled, { css } from 'styled-components/native';
 import type { Theme } from '../../styles/theme';
 import { Typography } from './Typography';
 
-const Title = styled(Typography)(
+const SubTitle = styled(Typography)(
   ({ theme }: { readonly theme: Theme }) => css`
     font-size: ${theme.fonts.sizes.m};
     font-weight: ${theme.fonts.weights.semiBold};
   `,
 );
 
-export { Title };
+export { SubTitle };
